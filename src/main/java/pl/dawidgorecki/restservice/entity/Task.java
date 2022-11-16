be37ac9712a879c85b54008658b36f8b6f941a60
Dawid Górecki <kontakt.dawidgorecki@gmail.com>
2022-11-16 17:51:37 +0100

    Add ModelMapper, create entity

---
+++ src/main/java/pl/dawidgorecki/restservice/entity/Task.java
@@ -0,0 +1,41 @@
+package pl.dawidgorecki.restservice.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = Task.TABLE_NAME)
+@Getter
+@Setter
+@NoArgsConstructor
+public class Task {
+    public static final String TABLE_NAME = "tasks";
+    public static final String COLUMN_PREFIX = "t_";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = COLUMN_PREFIX + "id", nullable = false)
+    private int id;
+
+    @Column(name = COLUMN_PREFIX + "name", nullable = false)
+    private String name;
+
+    @Column(name = COLUMN_PREFIX + "description", nullable = false)
+    private String description;
+
+    @Column(name = COLUMN_PREFIX + "deadline", nullable = false)
+    private LocalDate deadline;
+
+    @Column(name = COLUMN_PREFIX + "is_done", nullable = false)
+    private boolean isDone = false;
+
+    public Task(String name, String description, LocalDate deadline) {
+        this.name = name;
+        this.description = description;
+        this.deadline = deadline;
+    }
+}
